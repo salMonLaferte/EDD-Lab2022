2761fcb59a2f0bda423c9a3e0f362ba7f8742c86
salMonLaferte <david_diaz@ciencias.unam.mx>
2022-03-31 23:45:19 -0600

    Update Cola.java

--- edd/src/Estructuras/Cola.java
+++ edd/src/Estructuras/Cola.java
@@ -9,7 +9,7 @@ public class Cola<T> extends PushPop<T>{
         Nodo nuevoElemento = new Nodo(elemento);
         nuevoElemento.siguiente = null;
         //Si la cola esta vacia entonces la cabeza es el nuevo elemento, si no se inserta despues de ultimo.
-        if(isEmpty()){
+        if(this.isEmpty()){
             cabeza = nuevoElemento;
         }else{
             ultimo.siguiente = nuevoElemento;
@@ -26,9 +26,28 @@ public class Cola<T> extends PushPop<T>{
     }
 
     @Override
+    /**
+     * Regresa una representación de la cola en cadena de caracteres.
+     */
     public String toString() {
-        // TODO Auto-generated method stub
-        return null;
+        if(this.isEmpty()){
+            return "";
+        }
+        //Crea una pila y mete las representaciones de los elementos en cadenas de caracteres de la lista.
+        Pila<String> pila = new Pila<String>();
+        Nodo actual = cabeza;
+        while(actual != null){
+            pila.push(actual.toString());
+            actual = actual.siguiente;
+        }
+        //Va sacando los elementos en cadenas de caracteres para guardarlos en la cadena resultado con el orden inverso al de la lista.
+        String resultado = "";
+        while(pila.size() > 1){
+            resultado += pila.peek() + ", ";
+            pila.pop(); 
+        }
+        resultado += pila.peek();
+        return resultado;
     }
     
 }
