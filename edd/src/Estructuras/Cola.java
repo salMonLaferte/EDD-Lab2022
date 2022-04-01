434dbb5713f6dd2a56db4709a821f6177aa42d29
salMonLaferte <david_diaz@ciencias.unam.mx>
2022-03-31 23:19:09 -0600

    Update Cola.java

--- edd/src/Estructuras/Cola.java
+++ edd/src/Estructuras/Cola.java
@@ -1,5 +1,34 @@
 package edd.src.Estructuras;
 
-public class Cola {
+public class Cola<T> extends PushPop<T>{
+    @Override
+    /**
+     * Agregar al final
+     */
+    public void push(T elemento) {
+        Nodo nuevoElemento = new Nodo(elemento);
+        nuevoElemento.siguiente = null;
+        //Si la cola esta vacia entonces la cabeza es el nuevo elemento, si no se inserta despues de ultimo.
+        if(isEmpty()){
+            cabeza = nuevoElemento;
+        }else{
+            ultimo.siguiente = nuevoElemento;
+        }
+        //El nuevo elemento es el ùltimo y la cola aumenta su tamaño 
+        ultimo = nuevoElemento;
+        longi++;
+    }
+
+    @Override
+    public PushPop<T> clone() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
