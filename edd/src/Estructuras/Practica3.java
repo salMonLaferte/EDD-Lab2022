38637812972a397517bb62155fd350a419ae338d
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-04-21 18:22:21 -0500

    practica3

---
+++ edd/src/Estructuras/Practica3.java
@@ -0,0 +1,17 @@
+package edd.src.Estructuras;
+public class Practica3 {
+    
+    public void sumaCercana(Lista lista, int N){
+
+    }
+    public void permutacionesCadena(String cadena){
+
+    }
+    public void primosQueSuman(int S, int P, int N){
+
+    }
+    public void N_Reinas(int N){
+
+    }
+
+}
